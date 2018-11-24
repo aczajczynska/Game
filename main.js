424ad33dd77061982e99446404d0723ee73df78c
aczajczynska <czajczynska.aleksandra@gmail.com>
2018-11-24 17:16:25 +0100

    end functions

--- main.js
+++ main.js
@@ -39,6 +39,49 @@ function checkResult(player, ai){
         return "loss"
     }
 }
+ 
+function publishResult(player, ai, result){
+    document.querySelector('[data-summary="your-choice"]')
+    .textContent = player;
+
+    document.querySelector('[data-summary="ai-choice"]')
+    .textContent = ai;
+
+    document.querySelector('p.numbers span')
+    .textContent = ++gameSummary.numbers;
+
+    if(result === "win") {
+    document.querySelector('p.wins span')
+    .textContent = ++gameSummary.wins;
+
+    document.querySelector('[data-summary="who-win"]')
+    .textContent = "Gratulations, You won!";
+
+    document.querySelector('[data-summary="who-win"]').style.color="green";
+    } else if (result === "loss") {
+        document.querySelector('p.losses span')
+    .textContent = ++gameSummary.losses;
+
+    document.querySelector('[data-summary="who-win"]')
+    .textContent = "Try again!";
+
+    document.querySelector('[data-summary="who-win"]').style.color="red";
+    } else {
+     document.querySelector('p.draws span')
+    .textContent = ++gameSummary.draws;
+
+    document.querySelector('[data-summary="who-win"]')
+    .textContent = "Remis.";
+
+    document.querySelector('[data-summary="who-win"]').style.color="royalblue";
+    }
+}
+
+function resetData(){
+document.querySelector(`[data-option=${game.playerHand}]`).style.boxShadow = "";
+game.playerHand = "";
+game.aiHand = "";
+}
 
 
 function startGame() {
@@ -47,9 +90,9 @@ if(!game.playerHand) {
 }
 game.aiHand = aiChoise();
 const gameResult = checkResult(game.playerHand, game.aiHand);
-
+publishResult(game.playerHand, game.aiHand, gameResult);
+resetData();
 }
-
 
 
 hands.forEach(hand => hand.addEventListener('click',
